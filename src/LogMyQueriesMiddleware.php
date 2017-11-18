a66e70dcfc184e3aa4bc72478d919b5a6e87c366
Shyam Makwana <shyam.makwana18@gmail.com>
2017-11-18 23:38:10 +0530

    alpha version 0.1

---
+++ src/LogMyQueriesMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace technoknol\LogMyQueries;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Closure;
+
+class LogMyQueriesMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        DB::connection()->enableQueryLog();
+        return $next($request);
+    }
+
+    public function terminate() {
+
+        Log::debug('LogMyQueries_STARTED');
+        $queries = DB::getQueryLog();
+        foreach ($queries as $query) :
+            $prep = $query['query'];
+            foreach ($query['bindings'] as $binding) :
+                if(is_string($binding)) {
+                    $binding = '"'. $binding . '"';
+                }
+                $prep = preg_replace("#\?#", $binding, $prep, 1);
+            endforeach;
+            Log::debug($prep);
+        endforeach;
+        Log::debug('LogMyQueries_STARTED');
+    }
+}

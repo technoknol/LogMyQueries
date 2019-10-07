adc287404ee2c89698c6e95ffbd97ecb98d1312e
Technoknol <patel.gopal55555@gmail.com>
2019-10-07 07:01:54 +0000

    Apply fixes from StyleCI

--- src/LogMyQueriesMiddleware.php
+++ src/LogMyQueriesMiddleware.php
@@ -2,38 +2,40 @@
 
 namespace technoknol\LogMyQueries;
 
+use Closure;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Closure;
 
 class LogMyQueriesMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         DB::connection()->enableQueryLog();
+
         return $next($request);
     }
 
-    public function terminate() {
-
+    public function terminate()
+    {
         Log::debug('LogMyQueries_START');
         $queries = DB::getQueryLog();
         foreach ($queries as $query) :
             $prep = $query['query'];
-            foreach ($query['bindings'] as $binding) :
-                if(is_string($binding)) {
-                    $binding = '"'. $binding . '"';
+        foreach ($query['bindings'] as $binding) :
+                if (is_string($binding)) {
+                    $binding = '"'.$binding.'"';
                 }
-                $prep = preg_replace("#\?#", $binding, $prep, 1);
-            endforeach;
-            Log::debug($prep);
+        $prep = preg_replace("#\?#", $binding, $prep, 1);
+        endforeach;
+        Log::debug($prep);
         endforeach;
         Log::debug('LogMyQueries_END');
     }
